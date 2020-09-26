ed22e5be3fff54771b26b5b9129250414648988e
Kelsie He <hekelsie@gmail.com>
2020-09-25 18:34:47 -0600

    finish

--- University.java
+++ University.java
@@ -5,6 +5,9 @@ public class University
     public static void main(String[] args)
     {
         Scanner scan = new Scanner(System.in);
+        System.out.println("What is your last name?");
+        String lastname = scan.next();
+
         System.out.println("What is your SAT score?");
         boolean isOkay = true;
         int sat = scan.nextInt();
@@ -46,18 +49,25 @@ public class University
         }
 
         System.out.println("Where do you live?");
-        String home = scan.nextLine();
+        String home = scan.next();
         if (home == "Colorado") {
             elites = elites + 1;
-        } else if (home == "Florida")) {
+        } else if (home == "Florida") {
             isOkay = false;
         } else {
             accepts = accepts + 1;
         }
+        scan.close();
 
 
         if (isOkay) {
-            if ((elites >= 1) || (solids >= 2))
+            if ((elites >= 1) || (solids >= 2) || (lastname == "Padjen")) {
+                System.out.println("You meet the requirements! Welcome to Padjen university!");
+            } else {
+                System.out.println("Sorry, you do not meet the requirements.");
+            }
+        } else {
+            System.out.println("Sorry, one of your metrics is lower than acceptable.");
         }
 
 
